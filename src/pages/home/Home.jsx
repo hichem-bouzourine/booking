3979cdf9f99ce5adff6e139e48346d3c465d079d
Hichem <hichembouzourine.uni@gmail.com>
2022-08-26 00:21:27 +0100

    Add Homepage Lists.

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,11 +1,22 @@
+import Featured from "../../components/featured/Featured";
+import Inspirations from "../../components/featuredInspiration/Inspirations";
 import Header from "../../components/header/Header";
+import PropertyList from "../../components/PropertyList/PropertyList";
 import "./home.css";
 
 const Home = () => {
   return (
     <>
       <Header />
-      <div>Home</div>
+      <div className="homeContainer">
+        <Featured />
+        <div className="homeTitle">
+          <h1>Browse by property type</h1>
+          <PropertyList />
+          <h1>Get inspirations for your next trip</h1>
+          <Inspirations />
+        </div>
+      </div>
     </>
   );
 };
